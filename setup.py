d1298961490c8c15979e6a1602c634c5b3628466
TechCrafterGaming <markus-domaschke@gmx.de>
2024-04-05 10:03:11 +0200

    cleanup

--- setup.py
+++ setup.py
@@ -1,75 +1,27 @@
 # Run/Used from command -> pip install -e . / pip install -e . --config-settings editable_mode=strict
 from setuptools import setup, find_packages
-# from toml import load
-
-# #PEP 735 – Dependency Groups in pyproject.toml
-# pyproject = load('pyproject.toml')
-
-# base_dependencies = pyproject.get('dependency-groups', {}).get('base', [])
-# optional_dependencies = pyproject.get('dependency-groups', {}).get('dev', [])
-
-# setup(
-#     name='editor',
-#     version='0.1.0',
-#     packages=find_packages("src/editor"),
-#     package_dir={
-#         '': 'src/editor',
-#         'tests': 'src/editor/tests',
-#         'components': 'src/editor/components',
-#         'windows': 'src/editor/windows',
-#     },
-# )
-
-
-# setup(
-#     name='EBULA',
-#     version='0.1.0',
-#     packages=find_packages(
-#         where='.',
-#         include=['editor', 'editor.components']
-#         ),
-#     package_dir={
-#         # '': '.',
-#         # 'ebula': 'src',
-#         # 'editor': 'src/editor',
-#         'components': 'src/editor/components',
-#         # 'windows': 'src/editor/windows',
-
-#         # 'tests': 'src/editor/tests',
-#         # 'components': 'src/editor/components',
-#         # 'windows': 'src/editor/windows',
-#     },
-#     # install_requires=base_dependencies,
-#     # extras_require={
-#     #     'dev': optional_dependencies,
-#     # },
-# )
 
 setup(
-    # name="EBULA",
-    # version="0.1",
+    # name="EBULA",     #* -> from toml file automatically
+    # version="0.1",    #* -> from toml file automatically
     packages=
-        # find_packages(where=".",
-        #               include=["src"]) +
-        # find_packages(where="src",
-        #               include=["editor"]) +
-        # find_packages(where='src/editor',
-        #               include=["components", "windows"]),
-        find_packages(where=".") +
-        find_packages(where="src") +
-        find_packages(where='src/editor', exclude=["tests"]),
+        #* Include all that is specified
+        find_packages(where=".",
+                      include=["src"]) +
+        find_packages(where="src",
+                      include=["editor"]) +
+        find_packages(where='src/editor',
+                      include=["components", "windows"]),
+        #* Include all that is found
+        # find_packages(where=".") +
+        # find_packages(where="src") +
+        # find_packages(where='src/editor', exclude=["tests"]),
     package_dir={
+        #* Declarations for top levels and their paths
         '.': '.',       #? is needed because the src find overrides the src top level ??
         # 'src': 'src',   #? is not used ???
-        # '': '.',
-        # 'ebula': 'src',
         'editor': 'src/editor',
-        # 'components': 'src/editor/components',
-        # 'windows': 'src/editor/windows',
-
-        # 'tests': 'src/editor/tests',
         'components': 'src/editor/components',
         'windows': 'src/editor/windows',
     },
-
 )
