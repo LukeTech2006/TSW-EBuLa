e01dc5e9a9f37b8e593bcc2d20b10853204d9c94
TechCrafterGaming <markus-domaschke@gmx.de>
2024-04-05 03:35:19 +0200

    finally happy with the `setup.py` and the imports i am god at importing/setup.py now

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -5,6 +5,7 @@ SET venv.deactivate=%~dp0..\.venv\Scripts\deactivate.bat
 SET inst.base=%~dp0..\requirements\base.txt
 SET inst.dev=%~dp0..\requirements\dev.txt
 
+pip install toml
 @REM Setup Venv
 pip install virtualenv
 python -m venv %~dp0..\.venv
@@ -12,12 +13,18 @@ python -m venv %~dp0..\.venv
 @REM Enter & Install Venv
 call %venv.activate%
 cd %~dp0..
-pip install setuptools
-pip install toml
-pip install -e .
+
+@REM pip install -e .
+@REM https://setuptools.pypa.io/en/latest/userguide/development_mode.html#strict-editable-installs
+@REM Install as Package in edit mode
+pip install -e . --config-settings editable_mode=strict
+
+@REM Dependencies
 py -m pip install -r %inst.base%
 py -m pip install -r %inst.dev%
+
 call %venv.deactivate%
+
 
 echo.
 echo Finished Setup
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Run/Used from command -> pip install -e .
+# Run/Used from command -> pip install -e . / pip install -e . --config-settings editable_mode=strict
 from setuptools import setup, find_packages
 # from toml import load
 
@@ -8,12 +8,68 @@ from setuptools import setup, find_packages
 # base_dependencies = pyproject.get('dependency-groups', {}).get('base', [])
 # optional_dependencies = pyproject.get('dependency-groups', {}).get('dev', [])
 
+# setup(
+#     name='editor',
+#     version='0.1.0',
+#     packages=find_packages("src/editor"),
+#     package_dir={
+#         '': 'src/editor',
+#         'tests': 'src/editor/tests',
+#         'components': 'src/editor/components',
+#         'windows': 'src/editor/windows',
+#     },
+# )
+
+
+# setup(
+#     name='EBULA',
+#     version='0.1.0',
+#     packages=find_packages(
+#         where='.',
+#         include=['editor', 'editor.components']
+#         ),
+#     package_dir={
+#         # '': '.',
+#         # 'ebula': 'src',
+#         # 'editor': 'src/editor',
+#         'components': 'src/editor/components',
+#         # 'windows': 'src/editor/windows',
+
+#         # 'tests': 'src/editor/tests',
+#         # 'components': 'src/editor/components',
+#         # 'windows': 'src/editor/windows',
+#     },
+#     # install_requires=base_dependencies,
+#     # extras_require={
+#     #     'dev': optional_dependencies,
+#     # },
+# )
+
 setup(
-    name='your_package_name',
-    version='0.1.0',
-    packages=find_packages(),
-    # install_requires=base_dependencies,
-    # extras_require={
-    #     'dev': optional_dependencies,
-    # },
+    # name="EBULA",
+    # version="0.1",
+    packages=
+        # find_packages(where=".",
+        #               include=["src"]) +
+        # find_packages(where="src",
+        #               include=["editor"]) +
+        # find_packages(where='src/editor',
+        #               include=["components", "windows"]),
+        find_packages(where=".") +
+        find_packages(where="src") +
+        find_packages(where='src/editor', exclude=["tests"]),
+    package_dir={
+        '.': '.',       #? is needed because the src find overrides the src top level ??
+        # 'src': 'src',   #? is not used ???
+        # '': '.',
+        # 'ebula': 'src',
+        'editor': 'src/editor',
+        # 'components': 'src/editor/components',
+        # 'windows': 'src/editor/windows',
+
+        # 'tests': 'src/editor/tests',
+        'components': 'src/editor/components',
+        'windows': 'src/editor/windows',
+    },
+
 )
